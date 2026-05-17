4784d13cd504091c096ce6cfebad3a63cd23a6c4
khatereh <khaterehnaseri444@gmail.com>
2026-05-17 11:19:15 +0330

    refactor : remove formik and yup from project

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import IMG from "@/components/ui/customIMG/IMG";
+import React from "react";
 
 function Signup() {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="w-full h-152 relative">
+      <IMG src={"/assets/images/trapeze.png"} className="absolute bottom-0" />
+    </div>
+  );
 }
 
 export default Signup;
--- components/partial/registrationHeader/RegistrationHeader.tsx
+++ components/partial/registrationHeader/RegistrationHeader.tsx
@@ -1,22 +1,34 @@
+'use client'
 import Button from "@/components/ui/customButton/Button";
 import P from "@/components/ui/customP/P";
+import { usePathname,useRouter } from "next/navigation";
 
 function RegistrationHeader() {
-    return (
-        <div className="w-full h-30 flex items-center justify-center">
-            <div className="w-7xl h-10 flex items-center justify-between">
-                <P className="text-[32px] font-medium">تسک منیجر</P>
-                <div className="w-55 h-full flex items-center justify-between cursor-pointer">
-                    <P className="font-bold">
-                        ثبت‌نام نکرده‌ای؟
-                    </P>
-                    <Button className="w-24 h-10 rounded-md cursor-pointer text-white text-[14px] font-extrabold bg-[#208D8E]">
-                        ثبت نام
-                    </Button>
-                </div>
-            </div>
+  const router = useRouter();
+  const pathName = usePathname();
+  const isLogin = pathName.startsWith("/login");
+  const registerButtonHandler = () => {
+    if (isLogin) {
+      router.push("/signup");
+    } else {
+      router.push("/login");
+    }
+  };
+  return (
+    <div className="w-full h-30 flex items-center justify-center">
+      <div className="w-7xl h-10 flex items-center justify-between">
+        <P className="text-[32px] font-medium">تسک منیجر</P>
+        <div className="w-55 h-full flex items-center justify-between cursor-pointer">
+          <P className="font-bold">ثبت‌نام نکرده‌ای؟</P>
+          <Button 
+          onClick={registerButtonHandler}
+          className="w-24 h-10 rounded-md cursor-pointer text-white text-[14px] font-extrabold bg-[#208D8E]">
+            {isLogin ? " ثبت نام" : "ورورد"}
+          </Button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default RegistrationHeader;
