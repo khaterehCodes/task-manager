3e96d9836061c9e3019b5691afbcc3d232c211a3
khatereh <khaterehnaseri444@gmail.com>
2026-05-20 14:32:32 +0330

    feat : userInformation component is set in profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,20 @@
+'use client'
+import AccountInformation from "./(components)/AccountInformation";
+import Setting from "./(components)/Setting";
+import UserInformation from "./(components)/UserInformation";
+import { useProfileNav } from "./context/ProfileContext";
+
 function Profile() {
-    return (
-        <div>
-            profileee
+  const { selectedNav } = useProfileNav();
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+        <div className="w-[88%] h-auto">
+      {selectedNav === 1 && <UserInformation />}
+      {selectedNav === 2 && <AccountInformation />}
+      {selectedNav === 3 && <Setting />}
         </div>
-    );
+    </div>
+  );
 }
 
 export default Profile;
