ed02f7b7a3935ecfbe2a011bef536a1855830d9e
khatereh <khaterehnaseri444@gmail.com>
2026-05-21 09:44:32 +0330

    feat : add a onclick function for choose a color for theme

--- app/profile/setting/page.tsx
+++ app/profile/setting/page.tsx
@@ -1,9 +1,15 @@
+"use client";
 import Button from "@/components/ui/customButton/Button";
 import P from "@/components/ui/customP/P";
 import DarkMode from "@/components/ui/darkMode/DarkMode";
 import { themes } from "@/core/constants/global";
+import { useState } from "react";
 
 function Setting() {
+  const [colorId, setColorId] = useState<number>(9);
+  const getColorId = (id: number) => {
+    setColorId(id);
+  };
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <div className="w-[88%] h-auto">
@@ -14,11 +20,12 @@ function Setting() {
             <div className="w-fit flex items-center gap-3">
               {themes.map((c) => (
                 <Button
+                  onClick={() => getColorId(c.id)}
                   key={c.id}
-                  className={`rounded-full flex items-center justify-center cursor-pointer ${c.selected ? "w-8 h-8" : "w-5 h-5"}`}
+                  className={`rounded-full flex items-center justify-center cursor-pointer ${colorId === c.id ? "w-8 h-8" : "w-5 h-5"}`}
                   style={{ backgroundColor: c.color }}
                 >
-                  {c.selected && (
+                  {colorId === c.id && (
                     <div className="w-3 h-3 bg-white rounded-full"></div>
                   )}
                 </Button>
